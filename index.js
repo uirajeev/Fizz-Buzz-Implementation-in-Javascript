0009f64d04805752d5d01d7a67aff6e0bb1f8228
Rajeev Kumar Singh <rajeevkumar.web@gmail.com>
2020-01-20 17:08:35 +0000

    Solution: 2 added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ For each multiple of 5, print "Buzz" instead of the number.
 For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 */
 
-// Solution 
+// Solution: 1
 function FizzBuzz (num) {
   for (i = 1; i<= num; i ++) {
     if (i % 15 === 0) console.log('FizzBuzz');
@@ -21,5 +21,12 @@ function FizzBuzz (num) {
   // for(let i=0;i<100;)console.log((++i%3?'':'fizz')+(i%5?'':'buzz')||i)
 }
 
+// Solution: 2
+function FizzBuzzShort(num) 
+  // shortest way of the same Solution
+  for(let i=0;i<num;) console.log((++i%3?'':'Fizz')+(i%5?'':'Buzz')||i);
+}
 // Test Fizz Buzz
-FizzBuzz(50);
+FizzBuzz(20);
+
+FizzBuzzShort(20);
